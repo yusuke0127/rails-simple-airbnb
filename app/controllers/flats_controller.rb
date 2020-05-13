bf07fb3090b109773056953a05af736f3b59130b
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-13 23:19:32 +0900

    Implement add flat and the flats page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,28 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    if @flat
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
