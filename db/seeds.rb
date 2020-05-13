f365b1bb75f6728ee7546eb03744237d7538af2d
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-13 16:57:59 +0900

    Seeded some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Adding some flats"
+
+4.times do
+  Flat.create!(
+    name: Faker::Marketing.buzzwords,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(1000..10_000),
+    number_of_guests: rand(1..10)
+  )
+end
+
+
+puts "Created #{Flat.count} place to stay"
